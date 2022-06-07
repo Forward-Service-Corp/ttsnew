259da6a58c14a7a96864bbefde81d14471de7305
Andrew McCauley <uxmccauley@gmail.com>
2022-06-07 15:45:24 -0500

    Testing production next auth settings

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
+import {connectToDatabase} from "../../../lib/dbConnect";
+import {ObjectId} from "mongodb";
 
 export default NextAuth({
     adapter: MongoDBAdapter(clientPromise),
@@ -12,10 +14,49 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
         EmailProvider({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM
+            server: {
+                host: process.env.SMTP_HOST,
+                port: process.env.SMTP_PORT,
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD
+                }
+            },
+            from: process.env.SMTP_FROM
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    url: process.env.NEXTAUTH_URL
+    url: process.env.NEXTAUTH_URL,
+    callbacks: {
+        async signIn({user, account, profile, email, credentials}) {
+            if (user.hasOwnProperty("level")) {
+                return true
+            } else {
+
+                const {db} = await connectToDatabase()
+                await db.collection("users")
+                    .updateOne(
+                        {_id: ObjectId(user.id)},
+                        {
+                            $set: {
+                                level: "client"
+                            }
+                        }
+                    )
+
+                return true
+            }
+
+        },
+        async redirect({url, baseUrl}) {
+            return baseUrl
+        },
+        async session({session, user, token}) {
+            return session
+        },
+        async jwt({token, user, account, profile, isNewUser}) {
+            return token
+        }
+    }
+
 })
