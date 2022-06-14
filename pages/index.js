8daf87399385f16a2c74ab54e0c38beba7b688bf
Andrew McCauley <uxmccauley@gmail.com>
2022-06-14 14:53:19 -0500

    Replaced API call and whitelisted all IPs

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export async function getServerSideProps(context) {
     const baseUrl = req ? `${protocol}://${req.headers.host}` : ''
 
     // page data
-    const pageDataUrl = baseUrl + "/api/pages/indexPageData?userId=" + session?.user.email
+    const pageDataUrl = baseUrl + "/api/pages/indexPageData?userId=" + session.user.email
     const getPageData = await fetch(pageDataUrl)
     const pageDataJson = await getPageData.json()
 
