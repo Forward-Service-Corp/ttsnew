c2a038b3f8a6b04d643d3386f01b8c89bdbd6cf9
Andrew McCauley <uxmccauley@gmail.com>
2022-08-23 13:23:16 -0500

    reordered survey so that "this does not apply to me" is last

--- components/newLifeAreaSurveyForm.js
+++ components/newLifeAreaSurveyForm.js
@@ -2,15 +2,9 @@ import {lasList} from "../lib/lasList";
 import {labelMap} from "../lib/serviceLabelsMap";
 import Toggle from "./toggle";
 
-function NewLifeAreaSurveyForm({activeDomain, setAnswered, answered, domains, setDomains, setScore}) {
+function NewLifeAreaSurveyForm({activeDomain, setAnswered, answered, domains, setDomains}) {
 
-    function getScore() {
-        let score = 0
-        for (const key in answered) {
-            score += answered[key].selection
-        }
-        return score
-    }
+    const answerArr = [0,1,2,3,4,5]
 
     return (
         <div className={"p-0 md:p-4"}>
@@ -40,93 +34,24 @@ function NewLifeAreaSurveyForm({activeDomain, setAnswered, answered, domains, se
                     }))
                 }}>
 
-                    <div className={"flex justify-center align-middle"}>
-                        <div className={"flex-1 self-center"}>
-                            <input
-                                checked={answered[activeDomain] && answered[activeDomain].selection === 0}
-                                value={0} className={"hidden peer"} type={"radio"} id={activeDomain + "-0"}
-                                data-statement={"This does not apply to me."}
-                                name={activeDomain}/>
-                            <label
-                                className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300 mb-2 border text-sm"}
-                                htmlFor={activeDomain + "-0"}>This does not apply to me.</label>
-                        </div>
-                        <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
-                            <div className={"text-2xl"}>0</div>
-                        </div>
-                    </div>
-
-
-                    <div className={"flex justify-center align-middle"}>
-                        <div className={"flex-1 self-center"}>
-                            <input checked={answered[activeDomain] && answered[activeDomain].selection === 1} value={1}
-                                   className={"hidden peer"} type={"radio"} id={activeDomain + "-1"}
-                                   data-statement={lasList[activeDomain].statements[1]}
-                                   name={activeDomain}/>
-                            <label
-                                className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300 mb-2 border text-sm"}
-                                htmlFor={activeDomain + "-1"}>{lasList[activeDomain].statements[1]}</label>
-                        </div>
-                        <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
-                            <div className={"text-2xl"}>1</div>
-                        </div>
-                    </div>
-
-                    <div className={"flex justify-center align-middle"}>
-                        <div className={"flex-1 self-center"}>
-                        <input checked={answered[activeDomain] && answered[activeDomain].selection === 2} value={2}
-                               className={"hidden peer"} type={"radio"} id={activeDomain + "-2"}
-                               data-statement={lasList[activeDomain].statements[2]}
-                               name={activeDomain}/>
-                        <label className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300  mb-2 border text-sm"}
-                               htmlFor={activeDomain + "-2"}>{lasList[activeDomain].statements[2]}</label>
-                        </div>
-                        <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
-                            <div className={"text-2xl"}>2</div>
-                        </div>
-                    </div>
-
-                    <div className={"flex justify-center align-middle"}>
-                        <div className={"flex-1 self-center"}>
-                        <input checked={answered[activeDomain] && answered[activeDomain].selection === 3} value={3}
-                               className={"hidden peer"} type={"radio"} id={activeDomain + "-3"}
-                               data-statement={lasList[activeDomain].statements[3]}
-                               name={activeDomain}/>
-                        <label className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300  mb-2 border text-sm"}
-                               htmlFor={activeDomain + "-3"}>{lasList[activeDomain].statements[3]}</label>
-                        </div>
-                        <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
-                            <div className={"text-2xl"}>3</div>
-                        </div>
-                    </div>
-
-                    <div className={"flex justify-center align-middle"}>
-                        <div className={"flex-1 self-center"}>
-                        <input checked={answered[activeDomain] && answered[activeDomain].selection === 4} value={4}
-                               className={"hidden peer"} type={"radio"} id={activeDomain + "-4"}
-                               data-statement={lasList[activeDomain].statements[4]}
-                               name={activeDomain}/>
-                        <label className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300  mb-2 border text-sm"}
-                               htmlFor={activeDomain + "-4"}>{lasList[activeDomain].statements[4]}</label>
-                        </div>
-                        <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
-                            <div className={"text-2xl"}>4</div>
-                        </div>
-                    </div>
-
-                    <div className={"flex justify-center align-middle"}>
-                        <div className={"flex-1 self-center"}>
-                        <input checked={answered[activeDomain] && answered[activeDomain].selection === 5} value={5}
-                               className={"hidden peer"} type={"radio"} id={activeDomain + "-5"}
-                               data-statement={lasList[activeDomain].statements[5]}
-                               name={activeDomain}/>
-                        <label className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300  mb-2 border text-sm"}
-                               htmlFor={activeDomain + "-5"}>{lasList[activeDomain].statements[5]}</label>
-                        </div>
-                        <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
-                            <div className={"text-2xl"}>5</div>
-                        </div>
-                    </div>
+                    {answerArr.map(index => {
+                        return (
+                            <div key={index} className={"flex justify-center align-middle"}>
+                                <div className={"flex-1 self-center"}>
+                                    <input checked={answered[activeDomain] && answered[activeDomain].selection === index} value={index}
+                                           className={"hidden peer"} type={"radio"} id={activeDomain + "-" + index}
+                                           data-statement={lasList[activeDomain].statements[index]}
+                                           name={activeDomain}/>
+                                    <label
+                                        className={"block p-2 hover:bg-gray-300 peer-checked:bg-green-300 mb-2 border text-sm"}
+                                        htmlFor={activeDomain + "-" + index}>{lasList[activeDomain].statements[index]}</label>
+                                </div>
+                                <div className={"flex justify-center items-center mb-2 bg-orange-600 text-white w-[50px]"}>
+                                    <div className={"text-2xl"}>{index}</div>
+                                </div>
+                            </div>
+                        )
+                    })}
 
                 </form>
             </div>
