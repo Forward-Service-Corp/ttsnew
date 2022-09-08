571e92966447499e56f45371104e27a10904a7c3
Andrew McCauley <uxmccauley@gmail.com>
2022-09-08 11:06:29 -0500

    Change page title to CARE Network, added searching notification

--- pages/directory.js
+++ pages/directory.js
@@ -15,8 +15,10 @@ export default function Directory({pageDataJson}) {
     const [domain, setDomain] = useState("")
     const [county, setCounty] = useState("")
     const domains = Object.keys(labelMap)
+    const [searching, setSearching] = useState(false)
 
     async function search() {
+        setSearching(true)
         const fetchSearch = await fetch("/api/search-directory", {
             method: "POST",
             headers: {
@@ -24,53 +26,57 @@ export default function Directory({pageDataJson}) {
             },
             body: JSON.stringify({keyword, domain, county})
         }).then(res => res.json())
+        await setSearching(false)
         await setLoadedServices(fetchSearch)
     }
 
     return (
         <Layout title={"CARE Network"} session={user}>
             <Head>
-                <title>TTS / Directory</title>
+                <title>TTS / CARE Network</title>
             </Head>
 
-            <div className={" p-3 bg-gray-200 rounded shadow"}>
+            <div className={" p-3 bg-gray-100 rounded shadow"}>
                 <form className={"grid grid-cols-1 md:grid-cols-4 gap-4 items-end"} onSubmit={(e) => {
                     e.preventDefault()
                     search().then()
                 }}>
                     <div className={""}>
                         <p className={"text-xs text-gray-500"}>Search by keyword</p>
-                        <input className={"w-full"} id={"searchField"} type={"text"} value={keyword} onChange={(e) => {
+                        <input className={"w-full rounded border-gray-300 text-xs"} id={"searchField"} type={"text"}
+                               value={keyword} onChange={(e) => {
                             setKeyword(e.target.value)
                         }}/>
                     </div>
                     <div className={""}>
                         <p className={"text-xs text-gray-500"}>Search by domain</p>
-                        <select id={"domainSelect"} className={"w-full"} onChange={(e) => {
-                            setDomain(e.target.value)
-                        }}>
+                        <select id={"domainSelect"} className={"w-full rounded border-gray-300 text-xs"}
+                                onChange={(e) => {
+                                    setDomain(e.target.value)
+                                }}>
                             <option value={""}></option>
                             {domains.map(domain => <option key={domain} value={domain}>{labelMap[domain]}</option>)}
                         </select>
                     </div>
                     <div className={""}>
                         <p className={"text-xs text-gray-500"}>Search by county</p>
-                        <select className={"w-full"} id={"countySelect"} onChange={(e) => {
-                            setCounty(e.target.value)
-                        }}>
+                        <select className={"w-full rounded border-gray-300 text-xs"} id={"countySelect"}
+                                onChange={(e) => {
+                                    setCounty(e.target.value)
+                                }}>
                             <option value={""}></option>
                             {WICountiesList.map(county => <option value={county} key={county}>{county}</option>)}
                         </select>
                     </div>
                     <div className={"flex items-center"}>
                         <button type={"submit"}
-                                className={"py-2 px-6 mr-2 text-white text-sm rounded bg-gradient-to-t from-orange-600 to-orange-400 disabled:bg-gradient-to-b disabled:from-gray-300 disabled:to-gray-400"}
+                                className={"py-[6px] px-6 mr-2 text-white text-sm rounded bg-orange-500 hover:bg-orange-600"}
                                 onClick={() => {
                                     setSearched(true)
                                 }}>Search
                         </button>
                         <button type={"reset"}
-                                className={"py-2 px-6 text-white text-sm rounded bg-gradient-to-t from-orange-600 to-orange-400 disabled:bg-gradient-to-b disabled:from-gray-300 disabled:to-gray-400"}
+                                className={"py-[6px] px-6 text-white text-sm rounded bg-orange-500 hover:bg-orange-600"}
                                 onClick={() => {
                                     setKeyword("")
                                     setDomain("")
@@ -82,7 +88,9 @@ export default function Directory({pageDataJson}) {
                     </div>
                 </form>
             </div>
-
+            <div className={`rounded bg-green-100 p-2 text-xs text-center mt-4 ${searching ? "visible" : "hidden"}`}>
+                Searching...
+            </div>
             <div className={`text-center p-4 ${searched ? "visible" : "hidden"}`}>
                 {loadedServices.length === 0 ? "There were no results" : loadedServices.length + " results"}
             </div>
