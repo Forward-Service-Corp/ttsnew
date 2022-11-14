a5ad0919f1a58b45d938657d88416c6a69d48c7b
Andrew McCauley <uxmccauley@gmail.com>
2022-11-14 09:49:49 -0600

    Fix phone bug in care network listings

--- pages/referral/[...referralId].js
+++ pages/referral/[...referralId].js
@@ -38,6 +38,53 @@ export default function ReferralId({pageDataJson, referralDataJson}) {
         setUserReferrals(fetchReferrals)
     }
 
+    const textInfoJSX = (data, label) => {
+        return (
+            <div>
+                <p className={"text-xs text-gray-500 capitalize"}>{label}:</p>
+                <div>{data || <span className={"text-gray-500"}>{`No ${label} listed`}</span>}</div>
+            </div>
+        )
+    }
+
+    const textEmailJSX = (data, label) => {
+        if(data){
+            return (
+                <div>
+                    <p className={"text-xs text-gray-500 capitalize"}>{label}:</p>
+                    <div>{<a href={`mailto:${data}`} className={"underline text-orange-500"}>
+                        {data}</a> || <span className={"text-gray-500"}>No {label} listed</span>
+                    }</div>
+                </div>
+            )
+        }else{
+            return (
+                <div>
+                    <p className={"text-xs text-gray-500 capitalize"}>{label}</p>
+                    <div><span className={"text-gray-500"}>No {label} listed</span></div>
+                </div>
+            )
+        }
+    }
+
+    const textWebsiteJSX = (data, label) => {
+        if(data){
+            return (
+                <div>
+                    <p className={"text-xs text-gray-500"}>Website</p>
+                    <div className={"truncate"}>{<a target={"_blank"} rel={"noreferrer"} className={"text-orange-500 underline"} href={data}>Visit website</a>}</div>
+                </div>
+            )
+        }else{
+            return (
+                <div>
+                    <p className={"text-xs text-gray-500"}>Website</p>
+                    <div className={"truncate"}>{<span className={"text-gray-500"}>No {label} listed</span>}</div>
+                </div>
+            )
+        }
+    }
+
     return (
         <Layout title={referralDataJson.name} session={user}>
             <Head>
@@ -61,68 +108,25 @@ export default function ReferralId({pageDataJson, referralDataJson}) {
             <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"}>
                 {/*column 1*/}
                 <div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Name</p>
-                        <div>{referralDataJson.name || <span className={"text-gray-500"}>No name listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Email</p>
-                        <div>{<a href={`mailto:${referralDataJson.contactEmail}`} className={"underline text-orange-500"}>{referralDataJson.contactEmail}</a> || <span className={"text-gray-500"}>No email listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Phone</p>
-                        <div>{referralDataJson.contactPhone || <span className={"text-gray-500"}>No phone number listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Contact</p>
-                        <div>{referralDataJson.contactName || <span className={"text-gray-500"}>No contact name listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Hours</p>
-                        <div>{referralDataJson.hours || <span className={"text-gray-500"}>No contact name listed</span>}</div>
-                    </div>
+                    {textInfoJSX(referralDataJson.phone, "phone")}
+                    {textInfoJSX(referralDataJson.hours, "hours")}
+                    {textWebsiteJSX(referralDataJson.url, "website")}
+                    {textInfoJSX(referralDataJson.requirements, "requirements")}
+                    {textInfoJSX(referralDataJson.needs, "need to bring")}
                 </div>
                 {/*column 2*/}
                 <div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Address</p>
-                        <div>{referralDataJson.street || <span className={"text-gray-500"}>No address listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>City</p>
-                        <div>{referralDataJson.city || <span className={"text-gray-500"}>No city listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>County</p>
-                        <div>{referralDataJson.county || <span className={"text-gray-500"}>No county listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>State</p>
-                        <div>{referralDataJson.state || <span className={"text-gray-500"}>No zip code listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Zip</p>
-                        <div>{referralDataJson.zip || <span className={"text-gray-500"}>No state listed</span>}</div>
-                    </div>
+                    {textInfoJSX(referralDataJson.street, "street")}
+                    {textInfoJSX(referralDataJson.city, "city")}
+                    {textInfoJSX(referralDataJson.county, "county")}
+                    {textInfoJSX(referralDataJson.state, "state")}
+                    {textInfoJSX(referralDataJson.zip, "zip code")}
                 </div>
                 {/*column 3*/}
                 <div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Website</p>
-                        <div className={"truncate"}>{<a target={"_blank"} rel={"noreferrer"} className={"text-orange-500 underline"} href={referralDataJson.url}>Visit website</a> || <span className={"text-gray-500"}>No website listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Requirements</p>
-                        <div>{referralDataJson.requirements || <span className={"text-gray-500"}>No requirements listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>Need to bring</p>
-                        <div>{referralDataJson.needs || <span className={"text-gray-500"}>No items to bring listed</span>}</div>
-                    </div>
-                    <div>
-                        <p className={"text-xs text-gray-500"}>UID</p>
-                        <div>{referralDataJson._id}</div>
-                    </div>
+                    {textInfoJSX(referralDataJson.contactName, "contact person")}
+                    {textInfoJSX(referralDataJson.contactPhone, "contact person phone")}
+                    {textEmailJSX(referralDataJson.contactEmail, "contact person email")}
                 </div>
             </div>
         </Layout>
