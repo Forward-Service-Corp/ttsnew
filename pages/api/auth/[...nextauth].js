9e7451aa967a3c7a8bb9a26b73d1a83d03b5485b
Andrew McCauley <uxmccauley@gmail.com>
2023-03-02 13:56:56 -0600

    SignIn logic to detect FSC email addresses and set user level to coach

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,13 +3,14 @@ import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
+import {connectToDatabase} from "../../../lib/dbConnect";
 
 export default NextAuth({
     adapter: MongoDBAdapter(clientPromise),
     providers: [
         EmailProvider({
             server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
+            from: process.env.EMAIL_FROM
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
@@ -19,12 +20,20 @@ export default NextAuth({
     secret: process.env.NEXTAUTH_SECRET,
     url: process.env.NEXTAUTH_URL,
     callbacks: {
-        async signIn({user, account, profile, email, credentials}){
-            if(!user?.level){
-                user.level = "client"
-                user.timestamp = new Date()
-            }
+        async signIn({user, account, profile, email, credentials}) {
+            user.timestamp = new Date()
             return true
+        }
+    },
+    events: {
+        signIn: async ({user, isNewUser}) => {
+            if (isNewUser) {
+                const userEmail = user.email
+                const isFSCEmail = user.email.indexOf("fsc-corp.org") > -1
+                const {db} = await connectToDatabase()
+                await db.collection("users").updateOne({"email": userEmail}, {$set: {"level": isFSCEmail ? "coach" : "client"}})
+            }
+
         }
     }
 })
