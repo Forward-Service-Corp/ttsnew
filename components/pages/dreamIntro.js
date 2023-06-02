9f110180d6335d17fafe9df464479c80eb745546
Andrew McCauley <uxmccauley@gmail.com>
2023-06-02 00:19:11 -0500

    Change your to my

--- components/pages/dreamIntro.js
+++ components/pages/dreamIntro.js
@@ -2,7 +2,7 @@ function DreamIntro() {
     return (
         <div className={" bg-gray-100 p-4"}>
             <div>
-                <h1 className={"text-2xl text-orange-600 uppercase font-light"}>Your Dreams Today</h1>
+                <h1 className={"text-2xl text-orange-600 uppercase font-light"}>My Dreams Today</h1>
                 <p className={"text-sm"}>This is your Map of your Dreams. As you take the time to think about your
                     dreams and hopes let your heart
                     speak to you. <strong>Believe in what could be</strong>.</p>
