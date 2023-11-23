82ad00905a6ec8c5ab40ec30350258c24d49c3a7
Andrew McCauley <dmccauley@fsc-corp.org>
2023-11-23 15:43:53 -0600

    Removed and added back node modules

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     remotePatterns: [
