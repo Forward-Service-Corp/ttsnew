714f06c80c2d7ffe62f6ac650d1bb0d2dc974877
Andrew McCauley <dmccauley@fsc-corp.org>
2024-09-19 13:36:52 -0500

    Training banner

--- components/layout.js
+++ components/layout.js
@@ -45,13 +45,15 @@ export default function Layout({children, title, session, loadingState, version,
             setEnvironment("dev")
         }else if(location.indexOf("-test") > -1){
             setEnvironment("testing")
+        }else if(location.indexOf("-training")){
+            setEnvironment("training")
         }
     }, [environment])
 
 
     return (
         <>
-            <div className={`${environment === "dev" || environment === "testing" ? "visible" : "hidden"} p-4 text-center text-xs bg-pink-600 dark:bg-purple-900 text-white font-light`}>
+            <div className={`${environment === "dev" || environment === "testing" || environment === "training" ? "visible" : "hidden"} ${environment === "testing" ? "bg-pink-600" : "bg-indigo-600"} p-4 text-center text-xs text-white font-light`}>
                 You are currently in the <strong className={`uppercase font-black`}>{environment}</strong> environment.
             </div>
             {simpleModal ? <SimpleModal title={simpleModalTitle} message={simpleModalMessage} label={simpleModalLabel}
