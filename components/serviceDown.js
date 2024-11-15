77fa24e076ebed34b2770fa220c4c421e206fb04
Drew McCauley <dmccauley@fsc-corp.org>
2024-11-15 15:55:29 -0600

    Service Down for deployment banner

--- components/serviceDown.js
+++ components/serviceDown.js
@@ -3,8 +3,7 @@ export default function ServiceDown() {
         <div className="bg-gradient-to-br from-pink-500 to-pink-800 p-8 font-extralight rounded-xl shadow-xl mb-9 text-white text-lg text-center">
             <span className={`text-3xl font-bold`}>Notice</span>
             <span className={`text-lg block mt-4`}>The Map of My Dreams Application will be unavailable this weekend
-                due to planned maintenance. Please watch for communications about new functionality that will be
-                available after this planned outage.</span>
+                due to planned maintenance.</span>
         </div>
     );
 }
