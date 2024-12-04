fdee91abc396b45aaacf68bc716558b60fbfc85b
drewfsc <dmccauley@fsc-corp.org>
2024-12-04 11:55:34 -0600

    Fix - todos not showing on care plans paqe

--- pages/api/pages/carePlansPageData.js
+++ pages/api/pages/carePlansPageData.js
@@ -20,6 +20,8 @@ export default async(req, res) => {
     const todos = await todosCursor.toArray()
     await todosCursor.close()
 
+    console.log(req.body)
+
     res.json({user, referrals, notes, todos})
 
 }
