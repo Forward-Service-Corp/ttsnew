1aaa12080c1e768512fe6226b649c741ae720631
dmccauley <uxmccauley@gmail.com>
2024-12-12 10:49:46 -0600

    Fix

--- pages/clients.js
+++ pages/clients.js
@@ -29,7 +29,7 @@ export async function getServerSideProps(context) {
     const userJson = await getUser.json()
 
     // clients data
-    const getUsersUrl = baseUrl + "/api/get-clients?coachId=" + context.query.coachId
+    const getUsersUrl = baseUrl + "/api/get-clients?coachId=" + ""
     const getUsers = await fetch(getUsersUrl)
     const usersJson = await getUsers.json()
 
